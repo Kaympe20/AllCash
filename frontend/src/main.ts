87cd07bdf33f5b6abcc9838393120a3b365544d7
Kayla Blosser <kayla.blosser@proton.me>
2025-05-05 15:34:51 -0400

    Add BudgetElement class

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,3 +4,14 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
+class BudgetElement {
+    title: string;
+    planned: number;
+    remaining: number;
+
+    constructor(title: string, planned: number, remaining: number) {
+        this.title = title;
+        this.planned = planned;
+        this.remaining = remaining;
+    }
+}
