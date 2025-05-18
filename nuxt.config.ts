2660653f8d2dad451826b706c646dc5283bcc830
Kayla Blosser <kayla.blosser@proton.me>
2025-05-18 18:53:14 -0400

    Add color-mode for light/dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/style.css'],
+
   nitro: {
     plugins: [
       "~/server/plugins/mongodb.ts",
       "~/server/plugins/seedDatabase.ts"
     ],
   },
+
   runtimeConfig: {
     mongodbUri: process.env.MONGODB_URI
-  }
+  },
+
+  modules: ["@nuxtjs/color-mode"]
 })
