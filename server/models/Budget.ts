5a3fd4cf3780fecc482cfea3f794ca48e21382e0
Kayla Blosser <kayla.blosser@proton.me>
2025-05-17 21:28:33 -0400

    Add order to schema

--- server/models/Budget.ts
+++ server/models/Budget.ts
@@ -3,12 +3,14 @@ import mongoose from 'mongoose';
 const BudgetElementSchema = new mongoose.Schema({
     title: { type: String, required: true },
     planned: { type: Number, required: true },
-    remaining: { type: Number, required: true }
+    remaining: { type: Number, required: true },
+    order: { type: Number, default: 0 }
 });
 
 const BudgetCardSchema = new mongoose.Schema({
     title: { type: String, required: true },
     income: { type: Boolean, default: false },
+    order: { type: Number, default: 0 },
     budgetElements: [BudgetElementSchema]
 }, { timestamps: true });
 
