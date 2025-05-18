eded1933f3df327e4e375c6a1536f9ea8a01dc1e
Kayla Blosser <kayla.blosser@proton.me>
2025-05-18 14:18:48 -0400

    Remove settings in preparation to add to users db

--- server/plugins/seedDatabase.ts
+++ server/plugins/seedDatabase.ts
@@ -1,27 +1,7 @@
 import { Nitro } from "nitropack";
 import { BudgetCard } from "../models/Budget";
-import { Settings } from "../models/Settings";
 
 export default async (_nitroApp: Nitro) => {
-    try {
-        if (await Settings.countDocuments() === 0) {
-            console.log('Seeding settings with default settings...');
-            
-            const defaultSettings = [
-                {
-                    light: true,
-                }
-            ];
-            
-            await Settings.insertMany(defaultSettings);
-            console.log('Settings seeded successfully!');
-        } else {
-            console.log('Settings already seeded. Skipping seeding process.');
-        }
-    } catch (e) {
-        console.error('Error seeding database:', e);
-    }
-
     //End if the instance is in production mode
     if (process.env.NODE_ENV !== 'development') {
         console.log('Skipping budget seeding because instance is in production mode.');
