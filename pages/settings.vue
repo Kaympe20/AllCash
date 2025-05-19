1f10002e04c8174b3387d82cec58eb6ac39c9383
Jadenlong <jadenlong376@gmail.com>
2025-05-18 21:20:24 -0700

    button start

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,11 +1,35 @@
+<script setup lang="ts">
+    const value = ref(true)
+
+</script>
+
+
 <template>
     <div class="card">
         <h1>Settings</h1>
     </div>
     <div class="card">
         <h3>Color Mode</h3>
-        <b-button class="button" varient="danger">Dark</b-button>
-        <b-button class="button" varient="danger">System</b-button>
-        <b-button class="button" varient="danger">Light</b-button>
-    </div>
+            <Ubutton class="button" id="darkbutton">Dark</Ubutton>
+            <Ubutton class="button">System</Ubutton>
+            <Ubutton  class="button" id="lightbutton">Light</Ubutton>
+            <USwitch v-model="value"
+            </div>
 </template>
+
+<style scoped>
+
+    #lightbutton{
+        background-color: white; 
+        color:black;
+    }
+
+    #darkbutton{
+        background-color: black; 
+        color:white;
+    }
+
+    .button:hover{
+    background-color: gray;
+    }
+</style>
