c142bd2471ad20a2ef33613b0b823604f3907a5e
Kayla Blosser <kayla.blosser@proton.me>
2025-05-29 10:08:41 -0400

    Refactor User.ts to have default data in seperate variable

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,26 +1,29 @@
 import mongoose from "mongoose";
 
+const defaultBudgetCard = [
+    {
+        title: "Income",
+        income: false,
+        order: 0,
+        budgetElements: [
+            {
+                title: "Salary",
+                planned: 100,
+                remaining: 100,
+                order: 0
+            }
+        ]
+    }
+]
+
+
 const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: false },
     lastName: { type: String, required: false },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    budgetCards: { type: [mongoose.Schema.Types.ObjectId], ref: 'BudgetCard', required: false, default: [
-        {
-            title: "Income",
-            income: false,
-            order: 0,
-            budgetElements: [
-                {
-                    title: "Salary",
-                    planned: 100,
-                    remaining: 100,
-                    order: 0
-                }
-            ]
-        }
-    ]},
+    budgetCards: { type: [mongoose.Schema.Types.ObjectId], ref: 'BudgetCard', required: false, default: defaultBudgetCard },
 }, { timestamps: true });
 
 export const User = mongoose.model('User', UserSchema);
