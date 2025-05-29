eecfb5ac0cc3fbb6fec4e038ae026cbd8a2e97a1
Kayla Blosser <kayla.blosser@proton.me>
2025-05-29 09:15:08 -0400

    Add default data for user budgetCards

--- server/models/User.ts
+++ server/models/User.ts
@@ -4,7 +4,21 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    budgetCards: { type: mongoose.Schema.Types.ObjectId, ref: 'BudgetCard', required: true },
+    budgetCards: { type: [mongoose.Schema.Types.ObjectId], ref: 'BudgetCard', required: false, default: [
+        {
+            title: "Income",
+            income: false,
+            order: 0,
+            budgetElements: [
+                {
+                    title: "Salary",
+                    planned: 100,
+                    remaining: 100,
+                    order: 0
+                }
+            ]
+        }
+    ] },
 }, { timestamps: true });
 
 export const User = mongoose.model('User', UserSchema);
