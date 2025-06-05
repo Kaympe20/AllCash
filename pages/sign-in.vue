14a40e93b8c2d8b900e93678d2fb4bca056e1fc4
Kayla Blosser <kayla.blosser@proton.me>
2025-06-05 08:35:34 -0400

    Create sign-in.vue

---
+++ pages/sign-in.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Sign-In Options:</p>
+        <button @click="signIn('github')">
+            Github
+        </button>
+        <button @click="signIn('credentials', demoCredentials)">
+            Username and Password
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+    definePageMeta({
+        layout: false,
+        auth: { unauthenticatedOnly: true, navigateAuthenticatedTo: '/' },
+    })
+
+    const { signIn } = useAuth();
+
+    const demoCredentials = { username: 'test', password: 'hunter2' };
+</script>
