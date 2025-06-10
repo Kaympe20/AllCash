baa3b61dea7664f3bde00cfa2cfc5604d4310231
Kayla Blosser <kayla.blosser@proton.me>
2025-06-10 13:52:16 -0400

    Refactor User.ts to store all user data in one schema

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
+const BudgetElementSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    planned: { type: Number, required: true },
+    remaining: { type: Number, required: true },
+    order: { type: Number, default: 0 }
+});
+
+const BudgetCardSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    income: { type: Boolean, default: false },
+    order: { type: Number, default: 0 },
+    budgetElements: [BudgetElementSchema]
+}, { timestamps: true });
+
 const defaultBudgetCard = [
     {
         title: "Income",
@@ -16,14 +30,14 @@ const defaultBudgetCard = [
     }
 ]
 
-
 const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: false },
     lastName: { type: String, required: false },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    budgetCards: { type: [mongoose.Schema.Types.ObjectId], ref: 'BudgetCard', required: false, default: defaultBudgetCard },
+    passwordHash: { type: String, required: true },
+    salt: { type: String, required: true },
+    budgetCards: {type: [BudgetCardSchema], default: defaultBudgetCard },
 }, { timestamps: true });
 
 export const User = mongoose.model('User', UserSchema);
