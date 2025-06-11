3b81c51c760e05f569556101aac988f7d7fff891
Kayla Blosser <kayla.blosser@proton.me>
2025-06-11 14:30:04 -0400

    Complete out sign-in page

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,16 +1,53 @@
 <template>
     <div>
-        <p>Sign-In Options:</p>
-        <button @click="signIn('credentials')">
-            Username and Password
+        <h1>Sign-In Options:</h1>
+
+        <p>Authenticated? {{ status }}</p>
+
+        <input type="text" placeholder="Username" v-model="userCredentials.username">
+        <input type="password" placeholder="Password" v-model="userCredentials.password">
+
+        <button @click="signIn('credentials', userCredentials)" class="button">
+            Sign In
+        </button>
+
+        <hr>
+
+        <button @click="() => signOut" class="button">
+            Sign Out
         </button>
     </div>
 </template>
 
 <script setup lang="ts">
+import { signOut } from 'next-auth/react';
+
+
     definePageMeta({
         auth: { unauthenticatedOnly: true, navigateAuthenticatedTo: '/' },
     })
 
-    const { signIn } = useAuth();
+    const { signIn , status} = useAuth();
+
+    const userCredentials = ref({
+        username: '',
+        password: '',
+    });
 </script>
+
+<style scope>
+input {
+    display: block;
+    margin: 5px auto;
+    border-radius: 5px;
+    border: none;
+    width: 90%;
+    max-width: 500px;
+}
+.dark-mode input {
+    background-color: slategrey;
+}
+.dark-mode input::placeholder {
+    color: rgba(255, 255, 255, 0.549);
+}
+</style>
