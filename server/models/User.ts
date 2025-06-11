ea9d0715e52d4e3189da47728d2b47a232ec2fce
Kayla Blosser <kayla.blosser@proton.me>
2025-06-11 17:32:18 -0400

    Rename password to passwordHash to be more descriptive

--- server/models/User.ts
+++ server/models/User.ts
@@ -37,7 +37,7 @@ const UserSchema = new mongoose.Schema({
     lastName: { type: String, required: false },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    passwordHash: { type: String, required: true },
     budgetCards: { type: BudgetCardSchema, required: false, default: defaultBudgetCard },
 }, { timestamps: true });
 
