5e3836215a22dba8832f8538eadf59db33ec1d7f
Kayla Blosser <kayla.blosser@proton.me>
2025-06-11 23:05:04 -0400

    Flesh out /api/sign-up

--- server/api/sign-up.ts
+++ server/api/sign-up.ts
@@ -2,11 +2,19 @@ import { User } from "../models/User";
 import bcrypt from "bcrypt";
 
 export default defineEventHandler(async (event) => {
-    const body = await readBody(event);
+    try {
+        const body = await readBody(event);
 
-    await User.create({
-        username: body.username,
-        email: body.email,
-        passwordHash: bcrypt.hash(body.password, 10),
-    })
+        await User.create({
+            username: body.username,
+            email: body.email,
+            passwordHash: await bcrypt.hash(body.password, 10),
+            budgetCards: [],
+        })
+
+        return { success: true };
+    } catch (error) {
+        console.error('Error during sign-up:', error);
+        return { success: false, error: error };
+    }
 })
